2eecb84bd0eb2d9cb21f56b036efe9929c4fef4b
Bryan Sullivan <bryan.sullivan@att.com>
2017-12-01 09:23:09 -0800

    Support VES deploy on centos
    JIRA: VES-2
    Reverified as working on ubuntu
    
    Change-Id: I9d2425f092137c7547b5eeeedd7bf34aa426f3eb
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

--- tools/demo_deploy.sh
+++ tools/demo_deploy.sh
@@ -29,7 +29,7 @@
 #.
 #. Prerequisites:
 #. - Ubuntu Xenial host for the admin server
-#. - Ubuntu Xenial server for master and worker nodes
+#. - Ubuntu Xenial or Centos 7 server for master and worker nodes
 #. - Password-less ssh key provided for node setup
 #. - hostname of selected master node in DNS or /etc/hosts
 #. - env variables set prior to running this script, as per ves-setup.sh
@@ -48,27 +48,30 @@
 #.
 #. Usage: on the admin server
 #. $ git clone https://gerrit.opnfv.org/gerrit/ves ~/ves
-#. $ bash ~/ves/tools/demo_deploy.sh <key> <master> <workers> [cloudify]
+#. $ cd ~/ves/tools
+#. $ bash demo_deploy.sh <key> <user> <master> <workers> [cloudify]
 #.   <key>: SSH key enabling password-less SSH to nodes
+#.   <user>: username on node with password-less SSH authorized
 #.   <master>: master node where core components will be installed
 #.   <workers>: list of worker nodes where collectd will be installed
 #.   cloudify: flag indicating to deploy VES core services via Cloudify
 
 key=$1
-master=$2
-workers="$3"
-cloudify=$4
+user=$2
+master=$3
+workers="$4"
+cloudify=$5
 
 eval `ssh-agent`
 ssh-add $key
 
 echo; echo "$0 $(date): Setting up master node"
 ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-  ubuntu@$master sudo rm -rf /tmp/ves
+  $user@$master sudo rm -rf /tmp/ves
 scp -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-  ~/ves ubuntu@$master:/tmp
+  ~/ves $user@$master:/tmp
 ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-  ubuntu@$master <<EOF
+  $user@$master <<EOF
   ves_host=$master
   export ves_host
   ves_mode=node
@@ -98,25 +101,30 @@ ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
 EOF
 
 scp -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-  ubuntu@$master:/tmp/ves/ves_env.sh ~/ves/.
-
-echo; echo "$0 $(date): VES Grafana dashboards are available at http://$master:3001 (login as admin/admin)"
+  $user@$master:/tmp/ves/ves_env.sh ~/ves/.
 
 nodes="$master $workers"
 for node in $nodes; do
   echo; echo "$0 $(date): Setting up collectd at $node"
   if [[ "$node" != "$master" ]]; then
     scp -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-      ~/ves ubuntu@$node:/tmp
+      ~/ves $user@$node:/tmp
   fi
   ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-    ubuntu@$node <<EOF > /dev/null 2>&1 &
+    $user@$node <<EOF > /dev/null 2>&1 &
   ves_kafka_host=$master
   export ves_kafka_host
+  ves_kafka_port=$ves_kafka_port
+  export ves_kafka_port
   ves_kafka_hostname=$ves_kafka_hostname
   export ves_kafka_hostname
+  ves_mode=node
+  export ves_mode
   ves_collectd=build
   export ves_collectd
   bash /tmp/ves/tools/ves-setup.sh collectd
 EOF
 done
+
+echo; echo "$0 $(date): VES Grafana dashboards are available at http://$master:3000 (login as admin/admin)"
+
